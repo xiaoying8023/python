dc4caf451f8af44050869cbe57fcef7f2987f3ec
Administrator <wutongyejian@163.com>
2019-11-18 11:47:56 +0800

    hello

---
+++ hello/hello.py
@@ -0,0 +1,34 @@
+ 第一个注释
+print("hello,python")
+print("hello,world")
+# 记录账号
+number = "123"
+# 记录密码
+password = "123"
+print(password, number)
+price = 8.5
+weight = 7.5
+money = price * weight
+money = money - 5
+print(money)
+"""
+姓名：小明
+年龄：18
+性别：男生
+身高：175
+体重：150
+"""
+name = "小明"
+age = 18
+sex = "男"
+ss = True
+height = "175"
+weight = "187"
+print(name,age,sex,height,weight,ss)
+print(type(age))
+
+zhang = "张"
+san = "三"
+print(zhang+san)
+print((zhang+san)*10)
+input(zhang)
